1e9401ee0b85dd8131051b44d9a419ec49a6e5e7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "network_security_perimeter_associations" {
-  description = "All network_security_perimeter_association resources"
-  value       = azurerm_network_security_perimeter_association.network_security_perimeter_associations
-}
 output "network_security_perimeter_associations_access_mode" {
-  description = "List of access_mode values across all network_security_perimeter_associations"
-  value       = [for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : v.access_mode]
+  description = "Map of access_mode values across all network_security_perimeter_associations, keyed the same as var.network_security_perimeter_associations"
+  value       = { for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : k => v.access_mode }
 }
 output "network_security_perimeter_associations_name" {
-  description = "List of name values across all network_security_perimeter_associations"
-  value       = [for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : v.name]
+  description = "Map of name values across all network_security_perimeter_associations, keyed the same as var.network_security_perimeter_associations"
+  value       = { for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : k => v.name }
 }
 output "network_security_perimeter_associations_network_security_perimeter_profile_id" {
-  description = "List of network_security_perimeter_profile_id values across all network_security_perimeter_associations"
-  value       = [for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : v.network_security_perimeter_profile_id]
+  description = "Map of network_security_perimeter_profile_id values across all network_security_perimeter_associations, keyed the same as var.network_security_perimeter_associations"
+  value       = { for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : k => v.network_security_perimeter_profile_id }
 }
 output "network_security_perimeter_associations_resource_id" {
-  description = "List of resource_id values across all network_security_perimeter_associations"
-  value       = [for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : v.resource_id]
+  description = "Map of resource_id values across all network_security_perimeter_associations, keyed the same as var.network_security_perimeter_associations"
+  value       = { for k, v in azurerm_network_security_perimeter_association.network_security_perimeter_associations : k => v.resource_id }
 }
 
